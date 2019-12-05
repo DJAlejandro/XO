771d6098b5ea99679c9ab1e950a703e9619af9c5
unknown <siyangslg@163.com>
2019-12-05 16:03:45 +0800

    update

--- src/views/AlbumDetail.vue
+++ src/views/AlbumDetail.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="album-detail">
+    <div class="media-background">
+      <img :src="albumList.src" />
+    </div>
     <v-header></v-header>
-
     <div class="album-header" v-if="albumList!=null">
       <div class="album-img">
         <img :src="albumList.src" alt="edit" />
@@ -38,6 +40,41 @@
         </div>
       </div>
     </div>
+    <div class="media-container">
+      <div class="media-row header">
+        <div class="media-index">#</div>
+        <div class="media-title">title</div>
+        <div class="media-artist">artist</div>
+        <div class="media-time">time</div>
+        <div class="media-controls"></div>
+      </div>
+      <div
+        class="media-row"
+        v-for="(track,index) in albumList.tracks"
+        :class="{active:activeIndex===index}"
+        @click="changeIndex(index)"
+      >
+        <div class="media-index">
+          <div class="text">{{index+1}}</div>
+          <span class="play">
+            <span class="icon-play iconfont"></span>
+          </span>
+        </div>
+        <div class="media-title">{{track.name}}</div>
+        <div class="media-artist">
+          <span v-for="(artist,index) in track.artists">
+            <span v-if="index!==0">,</span>
+            {{artist.name}}
+          </span>
+        </div>
+        <div class="media-time">{{InstoreTime(track.time)}}</div>
+        <div class="media-controls">
+          <span class="icon-more iconfont"></span>
+          <span class="icon-add iconfont"></span>
+          <span class="icon-favourite iconfont"></span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -53,7 +90,8 @@ export default {
   props: {},
   data() {
     return {
-      albumList: {}
+      albumList: {},
+      activeIndex: 0
     };
   },
   components: {
@@ -70,6 +108,17 @@ export default {
       }
     }
   },
+  methods: {
+    InstoreTime(time) {
+      let msec = Math.floor(time / 1000) * 1000;
+      let minutes = parseInt(msec / (1000 * 60));
+      let seconds = parseInt((msec % (1000 * 60)) / 1000);
+      return minutes + ":" + seconds;
+    },
+    changeIndex(index) {
+      this.activeIndex = index;
+    }
+  },
   mounted() {
     let id = this.$route.query.id;
     instance
@@ -79,11 +128,11 @@ export default {
         }
       })
       .then(res => {
-        // console.log(res);
         let data = res.data;
         let tracks = [];
         let {
-          album: { blurPicUrl, name, artists, publishTime, songs }
+          album: { blurPicUrl, name, artists, publishTime },
+          songs
         } = data;
 
         songs.forEach(item => {
@@ -102,7 +151,6 @@ export default {
           publishTime, //发行时间
           tracks //歌曲
         };
-        console.log(this.albumList);
       });
   }
 };
@@ -113,6 +161,27 @@ export default {
   display: flex;
   flex-direction: column;
   position: relative;
+  .media-background {
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    position: absolute;
+    height: 100%;
+    z-index: -3;
+    transform: translateZ(0);
+    filter: blur(50px);
+
+    img {
+      width: 100%;
+      height: 100%;
+      opacity: 0.2;
+      position: absolute;
+      top: 0;
+      left: 0;
+      object-fit: cover;
+    }
+  }
   .album-header {
     display: flex;
     margin: 80px 0 48px;
@@ -199,6 +268,122 @@ export default {
           .icon-text {
             margin-left: 8px;
           }
+        }
+      }
+    }
+  }
+  .media-container {
+    margin: 0 28px;
+    margin-bottom: 48px;
+    min-height: 120px;
+
+    .media-row {
+      display: flex;
+      color: #fff;
+      overflow: hidden;
+      height: 48px;
+      width: 100%;
+      &:hover {
+        .media-index {
+          .play {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          .text {
+            display: none;
+          }
+        }
+      }
+      &.header {
+        div {
+          text-transform: uppercase;
+          font-size: 12px;
+          font-weight: 600;
+          line-height: 20px;
+          letter-spacing: 0.5px;
+          color: rgba(229, 238, 255, 0.6);
+        }
+      }
+      &.active {
+        background-color: rgba(246, 245, 255, 0.1);
+        outline: 0;
+      }
+      .media-index {
+        display: flex;
+        position: relative;
+        justify-content: center;
+        align-items: center;
+        flex: 0 0 42px;
+        width: 28px;
+        color: #79777f;
+        font-size: 14px;
+        @include ellipsis;
+        .text {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .play {
+          display: none;
+          position: absolute;
+          left: 0;
+          right: 0;
+          width: 100%;
+          height: 100%;
+          z-index: 2;
+          .icon-play {
+            font-size: 24px !important;
+            color: #fff;
+          }
+        }
+      }
+      .media-title {
+        display: flex;
+        align-items: center;
+        flex: 1 0 228px;
+        font-size: 14px;
+        padding-right: 16px;
+        @include ellipsis;
+      }
+      .media-artist {
+        display: flex;
+        align-items: center;
+        flex: 1 1 170px;
+        padding-right: 16px;
+        font-size: 14px;
+        @include ellipsis;
+      }
+      .media-time {
+        max-width: 77px;
+        flex: 1 1 66px;
+        display: flex;
+        text-align: right;
+        align-items: center;
+        padding-right: 16px;
+        font-size: 14px;
+        @include ellipsis;
+      }
+
+      .media-controls {
+        justify-content: flex-end;
+        flex: 0 0 100px;
+        display: flex;
+        align-items: center;
+        padding-right: 11px;
+        @include ellipsis;
+        span {
+          display: flex;
+          align-items: center;
+          min-width: 0;
+          justify-content: flex-end;
+          width: 24px;
+          height: 24px;
+          padding: 0;
+          flex-shrink: 0;
+          font-size: 24px;
+          color: rgba(229, 238, 255, 0.6);
+          transition: color 0.15s ease;
         }
       }
     }
